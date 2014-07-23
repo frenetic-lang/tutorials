e01c015352c17aa275c6cfa2d63e085dbf88039f
Nate Foster <jnfoster@cs.cornell.edu>
2014-07-23 16:11:22 -0400

    Routing solution refactor and Mininet scripts

--- ox-tutorial-solutions/Sol_Routing.ml
+++ ox-tutorial-solutions/Sol_Routing.ml
@@ -3,57 +3,75 @@ open OpenFlow0x01
 open OpenFlow0x01_Core
 open Network_Common
 
-module Network = Net
-module Topology = Network.Topology
+module Topology = Net.Topology
 
 module MyApplication = struct
 
   include OxStart.DefaultTutorialHandlers
 
-  let topology = Network.Parse.from_dotfile "topology.dot"
+  let topology = Net.Parse.from_dotfile "topology.dot"
 
-  let is_host topo v = 
-    match Node.device (Topology.vertex_to_label topo v) with 
+  let () = 
+    let fd = open_out "topology.py" in 
+    Printf.fprintf fd "%s" (Net.Pretty.to_mininet topology);
+    close_out fd
+
+  let is_host v = 
+    match Node.device (Topology.vertex_to_label topology v) with 
     | Node.Host -> true 
     | _ -> false  
   
   let hosts = 
-    Topology.VertexSet.filter (is_host topology) (Topology.vertexes topology)
+    Topology.VertexSet.filter is_host (Topology.vertexes topology)
 
-  let rules = 
+  let all_hops = 
+    let t = Topology.VertexHash.create 17 in 
+    Topology.VertexSet.iter
+      (fun h -> Topology.VertexHash.add t h (Net.UnitPath.all_shortest_paths topology h))
+      hosts;
+    t
+       
+  let prev_hop src curr = 
+    try 
+      let paths = Topology.VertexHash.find all_hops src in 
+      let prev = Topology.VertexHash.find paths curr in 
+      Some (Topology.edge_src (Topology.find_edge topology prev curr))
+    with _ -> 
+      None
+
+  let rule mac port = 
+    let pat = { match_all with dlDst = Some mac } in 
+    let acts = [Output (PhysicalPort(Int32.to_int port))] in 
+    (pat,acts)    
+
+  let path_rules src dst = 
+    let mac = Node.mac (Topology.vertex_to_label topology dst) in 
+    let rec loop src curr rules = 
+      if src = curr then rules 
+      else 
+	match prev_hop src curr with
+	| Some (prev,port) -> 
+	   let sw = Node.id (Topology.vertex_to_label topology prev) in 
+	   loop src prev ((sw, rule mac port)::rules)
+	| None -> rules in 
+    loop src dst []
+
+  let all_rules = 
     Topology.VertexSet.fold 
       (fun src rules ->          
-         let src_paths = Network.UnitPath.all_shortest_paths topology src in 
-         Topology.VertexSet.fold 
-	   (fun dst rules -> 
-	      if src = dst then rules
-	      else 		
-		begin
-		  let dst_addr = Node.ip (Topology.vertex_to_label topology dst) in 
-		  let rec loop curr next rules = 
-		    if curr = src then rules
-		    else 
-		      let sw = Node.id (Topology.vertex_to_label topology curr) in 
-		      let _,out_port = Topology.edge_src (Topology.find_edge topology curr next) in
-		      let rules' = (sw, dst_addr, Int32.to_int out_port)::rules in 
-		      loop (Topology.VertexHash.find src_paths curr) curr rules' in 
-		  loop (Topology.VertexHash.find src_paths dst) dst rules
-		end)
-	   hosts rules)
+       Topology.VertexSet.fold 
+	 (fun dst rules -> 
+	  if src = dst then rules
+	  else 		
+	    (path_rules src dst)@rules)
+	 hosts rules)
       hosts []
 		
   let switch_connected (sw: switchId) (feats : SwitchFeatures.t) : unit = 
     List.iter 
-      (fun (sw', ip, pt) -> 
-       if sw = sw' then
-	 let match_ip = 
-	   { match_all with 
-	     dlTyp = Some 0x800;
-	     nwDst = Some { m_value = ip; m_mask = None } } in 
-	 send_flow_mod sw 0l (add_flow 100 match_ip [Output (PhysicalPort pt)])
-       else ())
-      rules
-         
+      (fun (sw',(pat,acts)) -> 
+       if sw = sw' then send_flow_mod sw 0l (add_flow 100 pat acts))
+      all_rules
 
   let packet_in (sw : switchId) (xid : xid) (pk : packetIn) : unit =
     ()
