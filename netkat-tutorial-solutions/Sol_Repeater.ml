bd0b5cc9b18e0a8890f1f39c8fd763ef4dc409c9
Victoria Yang <vzy2@cornell.edu>
2014-08-07 10:26:48 -0400

    Update Sol_Repeater.ml

--- netkat-tutorial-solutions/Sol_Repeater.ml
+++ netkat-tutorial-solutions/Sol_Repeater.ml
@@ -2,8 +2,8 @@ open NetKAT.Std
 
 let connect m n =
   <:netkat<
-    filter (port = $m$); port := $n$ +
-    filter (port = $n$); port := $m$
+    filter (port = $m); port := $n +
+    filter (port = $n); port := $m
   >>
 
 let pol = connect 1l 2l
