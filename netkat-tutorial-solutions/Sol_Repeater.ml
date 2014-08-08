74196d7983bfa4848a9b51adace4b8c8ef682e7d
Victoria Yang <vzy2@cornell.edu>
2014-08-08 14:11:51 -0400

    Update Sol_Repeater.ml

--- netkat-tutorial-solutions/Sol_Repeater.ml
+++ netkat-tutorial-solutions/Sol_Repeater.ml
@@ -1,11 +1,4 @@
 open NetKAT.Std
-
-let connect m n =
-  <:netkat<
-    filter (port = $m); port := $n +
-    filter (port = $n); port := $m
-  >>
-
-let pol = connect 1l 2l
+open RepeaterPolicy
 
 let _ = run_static pol
