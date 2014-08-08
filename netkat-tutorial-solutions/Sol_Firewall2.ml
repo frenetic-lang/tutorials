556dd7f16218b34a3f3d3f3f2faae6e666f584b3
Victoria Yang <vzy2@cornell.edu>
2014-08-08 14:12:43 -0400

    Update Sol_Firewall2.ml

--- netkat-tutorial-solutions/Sol_Firewall2.ml
+++ netkat-tutorial-solutions/Sol_Firewall2.ml
@@ -1,17 +1,9 @@
 open NetKAT.Std
-
-let forwarding : policy =
-  <:netkat<
-   if ip4Dst = 10.0.0.1 then port := 1
-   else if ip4Dst = 10.0.0.2 then port := 2
-   else if ip4Dst = 10.0.0.3 then port := 3
-   else if ip4Dst = 10.0.0.4 then port := 4
-   else drop
-  >>
+open Forwarding
 
 let firewall : policy =
   <:netkat<
-    if ipProto = 0x01 && ethType = 0x800 then drop else $forwarding
+    if ipProto = 0x01 && ethType = 0x800 then drop else (filter ethType = 0x800; $forwarding)
   >>
 
 let _ = run_static firewall
