d57dc6a115af39ca6e128e000a9a9afffed1dd61
Victoria Yang <vzy2@cornell.edu>
2014-08-08 14:14:28 -0400

    Create Forwarding.ml

---
+++ netkat-tutorial-solutions/Forwarding.ml
@@ -0,0 +1,10 @@
+open NetKAT.Std
+
+let forwarding : policy =
+  <:netkat<
+   if ip4Dst = 10.0.0.1 then port := 1
+   else if ip4Dst = 10.0.0.2 then port := 2
+   else if ip4Dst = 10.0.0.3 then port := 3
+   else if ip4Dst = 10.0.0.4 then port := 4
+   else drop
+  >>
