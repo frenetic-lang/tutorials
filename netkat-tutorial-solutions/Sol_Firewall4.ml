6e2b22bb99d9a3e598b676c3cca3932b65ea316f
Victoria Yang <vzy2@cornell.edu>
2014-08-08 14:13:40 -0400

    Update Sol_Firewall4.ml

--- netkat-tutorial-solutions/Sol_Firewall4.ml
+++ netkat-tutorial-solutions/Sol_Firewall4.ml
@@ -1,23 +1,15 @@
 open NetKAT.Std
-
-let forwarding : policy =
-  <:netkat<
-   if ip4Dst = 10.0.0.1 then port := 1
-   else if ip4Dst = 10.0.0.2 then port := 2
-   else if ip4Dst = 10.0.0.3 then port := 3
-   else if ip4Dst = 10.0.0.4 then port := 4
-   else drop
-  >>
+open Forwarding
 
 let firewall : policy =
   <:netkat<
-    if (tcpSrcPort = 80 || tcpDstPort = 80) &&
-       (ip4Src = 10.0.0.1 && ip4Dst = 10.0.0.2 ||
-        ip4Src = 10.0.0.2 && ip4Dst = 10.0.0.1) ||
-       ipProto = 0x01 &&
-        (ip4Src = 10.0.0.3 && ip4Dst = 10.0.0.4 ||
-         ip4Src = 10.0.0.4 && ip4Dst = 10.0.0.3) then
-      $forwarding
+    if ((tcpSrcPort = 80 || tcpDstPort = 80) &&
+       (ip4Src = 10.0.0.1 && ip4Dst = 10.0.0.2 || 
+        ip4Src = 10.0.0.2 && ip4Dst = 10.0.0.1)) || 
+       ((ipProto = 0x01 && ethType = 0x800) && 
+       (ip4Src = 10.0.0.3 && ip4Dst = 10.0.0.4 ||
+        ip4Src = 10.0.0.4 && ip4Dst = 10.0.0.3)) then
+        (filter ethType = 0x800; $forwarding)
     else
       drop
   >>
