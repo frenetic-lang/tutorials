ae832763e10547208504c59af5bb9f7a54edd170
Victoria Yang <vzy2@cornell.edu>
2014-08-11 19:57:19 -0400

    Create Sol_Routing2.ml

---
+++ netkat-tutorial-solutions/Sol_Routing2.ml
@@ -0,0 +1,22 @@
+open NetKAT.Std
+open Firewall
+
+let forwarding : policy =
+  <:netkat<
+    if switch = 1 then
+      if port = 1 then port := 2
+      else if port = 2 then port := 1
+      else drop
+    else if switch = 2 then
+      if ethDst = 1 then port := 1
+      else if ethDst = 2 then port := 2
+  	  else port := 3
+    else if switch = 3 then
+      if ethDst = 3 then port := 1
+      else if ethDst = 4 then port := 2
+      else port := 3
+    else
+      drop
+   >>
+    
+let _ = run_static <:netkat< $firewall; $forwarding >>
