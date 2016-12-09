dbd2cb3a30c4a91a189f30c3e08bbe0c89e6d7fd
Sam Baxter <sbaxter@cs.umass.edu>
2016-12-09 15:20:00 -0500

    Update Repeater solutions

--- netkat-tutorial-solutions/Repeater.ml
+++ netkat-tutorial-solutions/Repeater.ml
@@ -1,22 +1,17 @@
-open Frenetic_NetKAT
 open Core.Std
 open Async.Std
 
 (* a simple repeater *)
-let repeater : policy =
-  <:netkat<
-    if port = 1 then port := 2 + port := 3 + port := 4
-    else if port = 2 then port := 1 + port := 3 + port := 4
-    else if port = 3 then port := 1 + port := 2 + port := 4
-    else if port = 4 then port := 1 + port := 2 + port := 3
-    else drop
-  >>
+let%nk repeater =
+  {| if port = 1 then port := 2 + port := 3 + port := 4
+     else if port = 2 then port := 1 + port := 3 + port := 4
+     else if port = 3 then port := 1 + port := 2 + port := 4
+     else if port = 4 then port := 1 + port := 2 + port := 3
+     else drop |}
 
-(*
-let _ =
-  let module Controller = Frenetic_NetKAT_Controller.Make in
-  Controller.start 6633;
-  Controller.update_policy repeater;
-  never_returns (Scheduler.go ());
+(* let _ = *)
+(*   let module Controller = Frenetic_NetKAT_Controller.Make (Frenetic_OpenFlow0x01_Plugin) in *)
+(*   Controller.start 6633; *)
+(*   Deferred.don't_wait_for (Controller.update repeater); *)
+(*   never_returns (Scheduler.go ()); *)
 
-*)
